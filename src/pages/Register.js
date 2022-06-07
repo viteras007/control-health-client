a2c72fe118d31054de70d7f16097458e6c326793
unknown <viteras10@gmail.com>
2022-06-07 16:57:26 -0300

    comitando alterações

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
-
+  const {isLoading, showAlert, displayAlert } = useAppContext()
+  
   const toggleMember = () => {
     setValues({
       ...values,
@@ -22,10 +23,16 @@ const Register = () => {
   };
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({...values, [e.target.name]: e.target.value })
   };
   const onSubmit = (e) => {
-    console.log(e.target);
+    e.preventDefault();
+    const {name, email, password, isMember } = values;
+    if(!email || !password || (!isMember && !name)){
+      displayAlert();
+      return;
+    }
+    console.log(values)
   };
 
   return (
@@ -33,7 +40,7 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Registrar"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {/* NAME FIELD */}
         {!values.isMember &&
           <FormRow 
